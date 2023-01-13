f5fd429fcfcfafb8061393a71ecf0281b1a9f022
oliver-wahome <olivernderitu135@gmail.com>
2023-01-13 08:06:11 +0300

    feat: homeContent basic ui complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,48 @@ function Home() {
 
       <div className="homeContent">
         <div className="homeCell cellOne">
-          <div className="Title">
+          <h1 className="cellOneRow Title">
+            The Easiest <br />
+            Way to Manage <br />
+            Your Finances <br />
+          </h1>
+          <p className="cellOneRow Description">
+            We are bringing banking application to a new level of
+            technology never seen before
+          </p>
 
-          </div>
-          <div className="Description">
-            
-          </div>
+          <form className="cellOneRow">
+            <input type="email" placeholder="Enter your email"/>
+            <input type="password" placeholder="Enter password" />
+            <button>subscribe</button>
+          </form>
         </div>
         <div className="homeCell cellTwo"></div>
-        <div className="homeCell">3</div>
-        <div className="homeCell">4</div>
+        <div className="homeCell cellThree"></div>
+
+        <div className="cellFour">
+          <div className="cellFourColumn">
+            <h3>4X</h3>
+            <p>
+              Rapid increase
+              of your wealth
+            </p>
+          </div>
+          <div className="cellFourColumn">
+            <h3>-30%</h3>
+            <p>
+              Decrease your 
+              expenses wisely
+            </p>
+          </div>
+          <div className="cellFourColumn">
+            <h3>1M+</h3>
+            <p>
+              Trusted regular 
+              active users
+            </p>
+          </div>
+        </div>
       </div>
 
       <Footer />
