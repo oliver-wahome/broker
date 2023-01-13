d4350c69b757e1690c9b5b19f08972c02885d68e
oliver-wahome <olivernderitu135@gmail.com>
2023-01-13 06:06:28 +0300

    feat: styling the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../componentStyles/Footer.css";
 
 function Footer() {
     let copyrightDate = new Date().getFullYear();
 
     return (
-        <div className="">
+        <div className="footer">
             <p>
                 All rights Reserved &copy; {copyrightDate}
             </p>
