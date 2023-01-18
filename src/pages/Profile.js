4ae7f9b1c203b5e26e6508b96e127e47c40f1940
oliver-wahome <olivernderitu135@gmail.com>
2023-01-18 23:47:33 +0300

    feat: checking if the user is logged in, in the profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,30 @@
 import React, { useEffect } from 'react';
-// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function Profile() {
+    let firstName = "";
+    let lastName = "";
+    let businessName = "";
+    let email = "";
 
     useEffect(() => {
-        console.log("component loaded");;
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                console.log(user.uid);
+            }
+            else {
+                console.log("user logged out");
+            }
+        }, []);
     })
 
     return(
         <div className="profile">
-            Profile Page
+            <h1>Profile Page</h1>
+            <p>{firstName} {lastName}</p>
+            <p>{businessName}</p>
+            <p>{email}</p>
         </div>
     );
 }
