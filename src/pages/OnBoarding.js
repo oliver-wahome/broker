e6a1d3814197d0b9fe01ce7fe18c507b6278a2c6
oliver-wahome <olivernderitu135@gmail.com>
2023-01-18 14:03:24 +0300

    feat: adding handleSubmit function onclick of onboarding page form button

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { useRef } from 'react';
 
 function OnBoarding(){
+    let firstName = useRef();
+    let lastName = useRef();
+    let businessName = useRef();
+    
+
+    const textStyle = {
+        margin: 0,
+        padding: 0,
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        let fName = firstName.current.value;
+        let lName = lastName.current.value;
+        let bName = businessName.current.value;
+
+        
+    }
+
+
     return(
-        <div className='onboarding'>
+        <div className='authPageContainer'>
+            <div className="authPage">
             <h1>OnBoarding Page</h1>
+                <form className="authForm">
+                    <p style={textStyle}>What's your name?</p>
+                    <input ref={firstName} type="text" placeholder="First name" autoFocus required/>
+                    <input ref={lastName} type="text" placeholder="Last name" required/>
 
-            <form className="authForm">
-                <p>What's your name?</p>
-                <input type="text" placeholder="First name" autoFocus required/>
-                <input type="text" placeholder="Last name" required/>
+                    <p style={textStyle}>What's your business name?</p>
+                    <input ref={businessName} type="text" placeholder="Your business name goes here" required/>
 
-                <p>What's your business name?</p>
-                <input type="text" placeholder="Your business name goes here" required/>
-
-                <p>What does your business do?</p>
-                {/* Insert a dropdown menu here */}
-                
-                <button className="authFormBtn">Continue to Broker</button>
-            </form>
+                    <p style={textStyle}>What does your business do?</p>
+                    {/* Insert a dropdown menu here */}
+                    
+                    <button className="authFormBtn">Continue to Broker</button>
+                </form>
+            </div>
         </div>
     );
 }
