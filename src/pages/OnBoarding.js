5ccd6ddc1b1d8361e18967a866cb20d2bf5f7b90
oliver-wahome <olivernderitu135@gmail.com>
2023-01-18 15:10:27 +0300

    feat: updating the user document in the firestore db from input from the onboarding form

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function OnBoarding(){
     let firstName = useRef();
@@ -16,8 +18,9 @@ function OnBoarding(){
     }
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+        event.preventDefault();//prevents reloading of the page
 
+        //getting the input data from the onboarding form
         let fName = firstName.current.value;
         let lName = lastName.current.value;
         let bName = businessName.current.value;
@@ -27,6 +30,16 @@ function OnBoarding(){
             if(user){
                 const uid = user.uid;
                 console.log(uid);
+
+                //update the user document to add more information
+                const userRef = doc(db, "users", uid);
+                updateDoc(userRef, {
+                    firstName: fName,
+                    lastName: lName,
+                    businessName: bName
+                });
+
+                navigate("/dashboard");
             }
             else {
                 console.log("not signed in");
@@ -41,19 +54,19 @@ function OnBoarding(){
         <div className='authPageContainer'>
             <div className="authPage">
             <h1>OnBoarding Page</h1>
-                <form className="authForm">
-                    <p style={textStyle}>What's your name?</p>
-                    <input ref={firstName} type="text" placeholder="First name" autoFocus required/>
-                    <input ref={lastName} type="text" placeholder="Last name" required/>
+            <form onSubmit={handleSubmit} className="authForm">
+                <p style={textStyle}>What's your name?</p>
+                <input ref={firstName} type="text" placeholder="First name" autoFocus required/>
+                <input ref={lastName} type="text" placeholder="Last name" required/>
 
-                    <p style={textStyle}>What's your business name?</p>
-                    <input ref={businessName} type="text" placeholder="Your business name goes here" required/>
+                <p style={textStyle}>What's your business name?</p>
+                <input ref={businessName} type="text" placeholder="Your business name goes here" required/>
 
-                    <p style={textStyle}>What does your business do?</p>
-                    {/* Insert a dropdown menu here */}
-                    
-                    <button className="authFormBtn">Continue to Broker</button>
-                </form>
+                <p style={textStyle}>What does your business do?</p>
+                {/* Insert a dropdown menu here */}
+                
+                <button className="authFormBtn">Continue to Broker</button>
+            </form>
             </div>
         </div>
     );
