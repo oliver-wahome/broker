4dcfcc70c09dfb398b427b0866f25c8a414911ea
oliver-wahome <olivernderitu135@gmail.com>
2023-01-19 09:12:44 +0300

    fix: solving re-render error when loading user profile component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,30 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Profile() {
-    let firstName = "";
-    let lastName = "";
-    let businessName = "";
-    let email = "";
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName: "",
+        businessName: "",
+        email: ""
+    });
+
+    let [executed, setExecuted] = useState({run: false});
+    //setExecuted(false);
+    let execCheck = executed.run;
 
     useEffect(() => {
         const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if(user){
-                console.log(user.uid);
-            }
-            else {
-                console.log("user logged out");
-            }
-        }, []);
-    })
+        if(!execCheck){
+            onAuthStateChanged(auth, async function(user) {
+                if(user){
+                    console.log(user.uid);
+                    //getting data from the firestore database
+                    const docRef = doc(db, "users", user.uid);
+                    const docSnap = await getDoc(docRef);
+    
+                    if(docSnap.exists()){
+                        console.log(docSnap.data());
+                        setUser({ 
+                            firstName: docSnap.data().firstName, 
+                            lastName: docSnap.data().lastName,
+                            businessName: docSnap.data().businessName,
+                            email: docSnap.data().email
+                        });
+                    }
+                    else {
+                        console.log("document data doesn't exist");
+                    }
+                }
+                else {
+                    console.log("user logged out");
+                }
+            }, []);
+
+            setExecuted(true);
+        }
+        else {
+            console.log("executed");
+        }
+        
+    }, [execCheck]);
 
     return(
         <div className="profile">
             <h1>Profile Page</h1>
-            <p>{firstName} {lastName}</p>
-            <p>{businessName}</p>
-            <p>{email}</p>
+            <p>{user.firstName} {user.lastName}</p>
+            <p>{user.businessName}</p>
+            <p>{user.email}</p>
         </div>
     );
 }
