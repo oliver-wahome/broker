62dde3e49c975e30fa39393572beb7639384277a
oliver-wahome <olivernderitu135@gmail.com>
2023-01-20 12:40:44 +0300

    fix: react-bootstrap modal works in income page

--- src/pages/Income.js
+++ src/pages/Income.js
@@ -1,42 +1,31 @@
 import React from 'react';
 import DashboardMenu from '../components/DashboardMenu';
+import AddIncome from '../components/AddIncome';
 import '../pageStyles/Dashboard.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../../node_modules/react-bootstrap/dist/react-bootstrap.js';
+// import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+
 
 function Income() {
-  return (
-    <div className="dashboardPage income">
 
-        <DashboardMenu />
+    return (
+        <div className="dashboardPage income">
+            <DashboardMenu />
 
-        <div className="dashboarBody">
-            <h1>Income Page</h1>
+            <div className="dashboarBody">
+                <h1>Income Page</h1>
 
-            <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Add Income
-            </button>
-        </div>
+                {/* <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add Income
+                </button> */}
 
-        {/* Add Income Modal form */}
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
+                <AddIncome />
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Income
+export default Income;
