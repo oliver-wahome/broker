9ec7bbfad03a8f1ba8c7560c9a5f3b7a3b4ca4b1
oliver-wahome <olivernderitu135@gmail.com>
2023-01-21 23:57:55 +0300

    update: removing the save changes button from add income modal

--- src/components/AddIncome.js
+++ src/components/AddIncome.js
@@ -46,9 +46,6 @@ function AddIncome() {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
