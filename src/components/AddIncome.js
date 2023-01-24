1a326fb2dfd7e45e9718725ca9d39fe50977a6f6
oliver-wahome <olivernderitu135@gmail.com>
2023-01-24 11:22:17 +0300

    feat: sending data from addIncome modal to subcollection in users collection

--- src/components/AddIncome.js
+++ src/components/AddIncome.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function AddIncome(props) {
     const [show, setShow] = useState(false);
@@ -10,10 +14,39 @@ function AddIncome(props) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    let navigate = useNavigate();
+    //initializing input references
+    let clientName = useRef();
+    let amount = useRef();
+    let date = useRef();
+    let time = useRef();
+    let description = useRef();
+
     const handleSubmit = (event) => {
         event.preventDefault();//preventing reloading of webpage onsubmit
         
-        console.log("handleSubmit working");
+        // console.log("handleSubmit working");
+        //checking if user is logged in and getting their uid
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                // adding the income collection within the document
+                addDoc(collection(db, "users", user.uid, "income"), {
+                    clientName: clientName.current.value,
+                    amount: amount.current.value,
+                    date: date.current.value,
+                    time: time.current.value,
+                    description: description.current.value,
+                    userId: user.uid
+                });
+            }
+            else {
+                console.log("not signed in");
+                navigate("/signin");
+            }
+        })
+
+        handleClose();
     }
 
     return (
@@ -30,27 +63,27 @@ function AddIncome(props) {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="clientName">
                             <Form.Label>Client Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter client name" />
+                            <Form.Control ref={clientName} type="text" placeholder="Enter client name" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="incomeAmount">
                             <Form.Label>Amount</Form.Label>
-                            <Form.Control type="number" placeholder="Enter income amount" />
+                            <Form.Control ref={amount} type="number" placeholder="Enter income amount" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="incomeDate">
                             <Form.Label>Date</Form.Label>
-                            <Form.Control type="date" />
+                            <Form.Control ref={date} type="date" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="incomeTime">
                             <Form.Label>Time</Form.Label>
-                            <Form.Control type="time" placeholder="Enter time of income payment" />
+                            <Form.Control ref={time} type="time" placeholder="Enter time of income payment" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="incomeDescription">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a description of the income" />
+                            <Form.Control ref={description} type="text" placeholder="Enter a description of the income" />
                         </Form.Group>
                         
                         <Button variant="primary" type="submit" className="float-end">
