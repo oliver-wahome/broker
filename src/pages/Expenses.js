1910fd8db6d731929a2618bdcb3629147aab4a6d
oliver-wahome <olivernderitu135@gmail.com>
2023-01-25 13:19:46 +0300

    feat: retrieving expense data from firestore in the expense page

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import DashboardNavbar from '../components/DashboardNavbar';
 import DashboardMenu from '../components/DashboardMenu';
 import AddExpenses from '../components/AddExpenses';
 import '../pageStyles/Dashboard.css';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
+import Table from 'react-bootstrap/Table';
 
 function Expenses() {
+    let navigate = useNavigate();
+
+    const [expense, setExpense] = useState([]);
+
+    useEffect(() => {
+        const auth = getAuth();
+
+        onAuthStateChanged(auth, async function(user){
+            if(user){
+                const expenseData = collection(db, "users", user.uid, "expense");
+                const querySnapshot = await getDocs(expenseData);
+
+                //looping through all expense subcollection documents
+                querySnapshot.forEach((doc) => {
+                    //pushing each document object to the expense state array
+                    setExpense(current => [...current, doc.data()]);
+                });
+            }
+            else {
+                console.log("user is not logged in");
+                navigate("/signin");
+            }
+        });
+    }, [navigate], [expense]);
+
     return (
         <div className="dashboardPage expenses">
             <DashboardNavbar />
