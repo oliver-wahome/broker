7033b94c58814207eaeaefb73bf03e947ab5fa98
oliver-wahome <olivernderitu135@gmail.com>
2023-01-31 19:53:12 +0300

    feat: redirecting dashboard menu link to registration page if the user isn't signed in

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,6 +3,8 @@ import DashboardNavbar from '../components/DashboardNavbar';
 import DashboardMenu from '../components/DashboardMenu';
 import '../pageStyles/Dashboard.css';
 import axios from 'axios';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
 
@@ -13,43 +15,57 @@ function Dashboard() {
         temp: 0
     });
 
+    let navigate = useNavigate();
+
     useEffect(() => {
-        let weather_api_url = '';        
 
-        //function to get the weather from theweatherapi using the location fetched from geoiplookup.io
-        function fetchWeather(){
-            axios.get(weather_api_url)
-                .then(res => {
-                    const currentWeather = res.data.current;
-                    setWeather({
-                        icon: currentWeather.condition.icon,
-                        text: currentWeather.condition.text,
-                        temp: currentWeather.temp_c
-                    });
-                })
-                .catch(error => {
-                    console.log(error.message);
-                })
-        }
+        const auth = getAuth();
+        //checking if the user is logged. If so, getting their userId
+        onAuthStateChanged(auth, async (user) => {
+            if(user){
+                let weather_api_url = '';        
 
-        //function to fetch location data from geoiplookup.io
-        //function is async so that location is got before weather is fetched since the weather is dependent on it
-        async function fetchLocation(){
-            await axios.get('https://json.geoiplookup.io/')
-                .then(res => {
-                    const currentCity = res.data.city;
-                    weather_api_url = 'http://api.weatherapi.com/v1/current.json?key=2e7f2c25ccc4417c80272145222312&q='+currentCity+'&aqi=yes';
-                    setCity(currentCity);
-                })
-                .catch((error) => {
-                    console.log(error.message);
-                })
-            
-            fetchWeather();
-        }
+                //function to get the weather from theweatherapi using the location fetched from geoiplookup.io
+                function fetchWeather(){
+                    axios.get(weather_api_url)
+                        .then(res => {
+                            const currentWeather = res.data.current;
+                            setWeather({
+                                icon: currentWeather.condition.icon,
+                                text: currentWeather.condition.text,
+                                temp: currentWeather.temp_c
+                            });
+                        })
+                        .catch(error => {
+                            console.log(error.message);
+                        })
+                }
 
-        fetchLocation();
-    }, []);
+                //function to fetch location data from geoiplookup.io
+                //function is async so that location is got before weather is fetched since the weather is dependent on it
+                async function fetchLocation(){
+                    await axios.get('https://json.geoiplookup.io/')
+                        .then(res => {
+                            const currentCity = res.data.city;
+                            weather_api_url = 'http://api.weatherapi.com/v1/current.json?key=2e7f2c25ccc4417c80272145222312&q='+currentCity+'&aqi=yes';
+                            setCity(currentCity);
+                        })
+                        .catch((error) => {
+                            console.log(error.message);
+                        })
+                    
+                    fetchWeather();
+                }
+
+                fetchLocation();
+            }
+            else {
+                console.log("user is not logged in");
+                navigate("/register");
+            }
+        });
+
+    }, [navigate]);
 
     return (
         <div className="dashboardPage">
